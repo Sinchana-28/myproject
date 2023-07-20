5917aa37a5824312c6bada8b9fb9abcad657973a
Sinchana-28 <sinchanapoojary28@gmail.com>
2023-07-20 23:44:52 +0530

    initial

--- fact.py
+++ fact.py
@@ -1,12 +1,12 @@
-#Author: OMKAR PATHAK
+
 #This program calculates the factorial of a given number
 
-def factorial(number):
+def factorial(num):
     '''This function calculates the factorial of a number'''
-    if number == 1 or number == 0:
+    if num == 1 or num == 0:
         return 1
     else:
-        return number * factorial(number - 1)
+        return num * factorial(num - 1)
 
 def factorial_without_recursion(number):
     fact = 1
